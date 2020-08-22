b1f46d7b7d60b166acda0fee13fa4f656ff4b702
Countra <1025276726@qq.com>
2020-08-22 16:31:40 +0800

    fix some small bugs

--- install_py38.sh
+++ install_py38.sh
@@ -40,10 +40,17 @@ tar -zxvf *.tgz
 cd Python-3.8.5
 ./configure --prefix=/usr/local/python3
 make && make install
-ln -s /usr/local/python3/bin/python3 /usr/bin/python3
+ln -s /usr/local/python3/bin/python3 /usr/bin/python38
+ln -s /usr/local/python3/bin/pip3 /usr/bin/pip38
 echo 'export PATH=$PATH:$HOME/bin:/usr/local/python3/bin' >> ~/.bash_profile
 source ~/.bash_profile
 
 echo -e "${OK} ${GreenBG} 安装完成 测试如下${Font}"
-python3 -V
-pip3 -V
+echo -e "${OK} ${GreenBG}38版本的python和pip的命令分别为python38和pip38${Font}"
+python38 -V
+pip38 -V
+
+# pip38安装一下一些必要的库
+/usr/local/python3/bin/python3.8 -m pip install --upgrade pip
+pip38 install httpx
+pip38 install translate
